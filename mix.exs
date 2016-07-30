da2474cc8fdc586be7d39e6fa8c6e037230796df
Melvin Lammerts <melvinrts@gmail.com>
2016-07-30 23:11:46 +0200

    Prepare hex package

--- mix.exs
+++ mix.exs
@@ -2,12 +2,24 @@ defmodule RandomPuppy.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :random_puppy,
-     version: "0.1.0",
-     elixir: "~> 1.1",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :random_puppy,
+      version: "0.1.0",
+      description: "Get a URL to a random puppy image.",
+      elixir: "~> 1.1",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      package: package(),
+      deps: deps()
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["MIT"],
+      maintainers: ["Melvin Lammerts"],
+      links: %{"GitHub" => "https://github.com/melvinsh/random_puppy"}
+    ]
   end
 
   def application do
